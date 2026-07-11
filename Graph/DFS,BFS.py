03dce921f8d2ccdf19a5fe3c82ddb0d765bdf0c6
Anurag Chauhan <anurag_c@ee.iitr.ac.in>
2026-07-11 19:50:45 +0530

    dfs bfs added

--- Graph/DFS,BFS.py
+++ Graph/DFS,BFS.py
@@ -1,5 +1,47 @@
+from collections import defaultdict, deque
 #Implementing DFS and BFS for a graph in all ways 
 
-#For graph given as edges 
+#For graph with n nodes from 0 to n-1 given as edges
+n = 6
+edges = [[0,1,1],[0,2,3],[1,3,4],[2,4,2],[4,5,1],[1,4,6],[0,5,8]]
 
+graph = defaultdict(list)
+
+for u,v,w in edges:
+    graph[u].append(v)           # Considering undirected graph
+    graph[v].append(u)
+
+vis = [0]*len(graph)
+
+def dfs(node):
+    vis[node] = 1
+    print(node)
+
+    for neighbor in graph[node]:
+        if vis[neighbor] == 0:
+            dfs(neighbor)
+
+for i in range(n):
+    if vis[i] == 0:
+        dfs(i)
+
+print("dfs done")
+
+vis = [0]*len(graph)
+
+def bfs(node):
+    q = deque([node])
+
+    while q:
+        j = q.popleft()
+        vis[j] = 1
+        print(j)
+
+        for neighbor in graph[j]:
+            if vis[neighbor] == 0:
+                q.append(neighbor)
+
+for i in range(n):
+    if vis[i] == 0:
+        bfs(i)
 
--- Graph/Dijkstra's.py
+++ Graph/Dijkstra's.py
@@ -1,6 +1,8 @@
 from collections import defaultdict, deque
 import heapq
 
+#Implementing Dijkstra's algorithm to find shortest path for all nodes from a particular node
+
 edges = [[0,1,1],[0,2,3],[1,3,4],[2,4,2],[4,5,1],[1,4,6],[0,5,8]]
 
 
@@ -11,16 +13,17 @@ for u,v,w in edges:
 
 distances = [float('inf')]*len(graph)
 
-start = 0    # Taking 0 as the starting point to find min distance from
+start = 0    # Taking 0 as the starting point, to find the min. distance from
 distances[start] = 0
 
 pq = []
-heapq.heappush(pq, (0,start))
+heapq.heappush(pq, (0,start)) 
+# input = (list, (distance,node)); (distance, node) to prioritise distance based sorting in heap
 
 while pq:
     distance, node = heapq.heappop(pq)
 
-    if distance > distances[node]:
+    if distance > distances[node]:         # If already smaller path present, then continue
         continue
 
     for weight, neighbor in graph[node]:
